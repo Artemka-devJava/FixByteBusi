5cf231cdb69742d61c0f9961447158c1c59fda20
411241020-blip <tarabakin.artem22@gmail.com>
2026-01-14 13:06:08 +0300

    Вроде всё наладил

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,8 @@ open module ru.fixbyte {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.xerial.sqlitejdbc;
 
-    // Разрешаем нативный доступ
-    requires static org.slf4j;
-
+    exports ru.fixbyte;
+    exports ru.fixbyte.controller;
+    exports ru.fixbyte.model;
 }
